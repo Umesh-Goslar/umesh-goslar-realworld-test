cb9f22380037a9396f7b22135e58a284fbdba78b
Umesh G <umesh.goslar@philips.com>
2021-02-01 14:52:56 +0530

    constatnts update

--- src/constants.ts
+++ src/constants.ts
@@ -5,4 +5,3 @@ export const APPLICATION_JSON = "application/json"
 
 /* API constants */
 export const COMMENTS_GET_URL = "https://jsonplaceholder.typicode.com/comments"
-export const ALBUMSPHOTOS_GET_URL = "https://jsonplaceholder.typicode.com/photos?albumId="
